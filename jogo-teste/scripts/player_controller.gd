511996c4d217362706d7991d030f622a7d66f2b5
Vitto-Mazeto <Vitto.Mazeto@sou.inteli.edu.br>
2023-02-06 22:07:32 -0300

    commit 1.0
    Jogo

---
+++ jogo-teste/scripts/player_controller.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+export var speed : int = 400 #variável da velocidade, o export coloca no menu
+export var jump_speed : int = -200 #velocidade do pulo
+export var gravity : int = 200 #gravidade
+var velocity = Vector2()
+
+func get_input(delta):
+	velocity.x = 0
+	if Input.is_action_pressed("move_right"):
+		velocity.x += speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= speed
+	if Input.is_action_pressed("jump"):
+		if (is_on_floor()):
+			velocity.y += jump_speed
+	
+	#gravity 
+	velocity.y += gravity * delta 
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+
+		
+
+
+func _physics_process(delta): # executa tantas vezes por segundo
+	get_input(delta)
+
