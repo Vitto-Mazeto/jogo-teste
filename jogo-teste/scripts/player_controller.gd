0485a40c30cabccb6c93ff977c0bae064c28abcc
Vitto-Mazeto <Vitto.Mazeto@sou.inteli.edu.br>
2023-02-06 22:37:59 -0300

    Commit 2.0
    Achei os sprites de industria, descobri como fazia os tilemap funcionarem

--- jogo-teste/scripts/player_controller.gd
+++ jogo-teste/scripts/player_controller.gd
@@ -5,12 +5,16 @@ export var jump_speed : int = -200 #velocidade do pulo
 export var gravity : int = 200 #gravidade
 var velocity = Vector2()
 
+onready var sprite = get_node("Sprite")
+
 func get_input(delta):
 	velocity.x = 0
 	if Input.is_action_pressed("move_right"):
 		velocity.x += speed
+		sprite.flip_h = false
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= speed
+		sprite.flip_h = true
 	if Input.is_action_pressed("jump"):
 		if (is_on_floor()):
 			velocity.y += jump_speed
